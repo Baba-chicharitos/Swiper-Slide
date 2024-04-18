8f33599b3f91ed0a79432670793aa8ba43f96dd0
Baba-chicharitos <baptiste.ser09@gmail.com>
2024-04-18 14:43:24 +0200

    Commit 11

--- main.js
+++ main.js
@@ -13,29 +13,75 @@ let po4 = document.getElementById("point4");
 let po5 = document.getElementById("point5");
 let y = 3;
 
-
 function reset() {
-  p1.classList.remove("droite1","droite2","droite3","droite4","centre","gauche1","gauche2","gauche3","gauche4");
-  p2.classList.remove("droite1","droite2","droite3","droite4","centre","gauche1","gauche2","gauche3","gauche4");
-  p3.classList.remove("droite1","droite2","droite3","droite4","centre","gauche1","gauche2","gauche3","gauche4");
-  p4.classList.remove("droite1","droite2","droite3","droite4","centre","gauche1","gauche2","gauche3","gauche4");
-  p5.classList.remove("droite1","droite2","droite3","droite4","centre","gauche1","gauche2","gauche3","gauche4");
+  p1.classList.remove(
+    "droite1",
+    "droite2",
+    "droite3",
+    "droite4",
+    "centre",
+    "gauche1",
+    "gauche2",
+    "gauche3",
+    "gauche4"
+  );
+  p2.classList.remove(
+    "droite1",
+    "droite2",
+    "droite3",
+    "droite4",
+    "centre",
+    "gauche1",
+    "gauche2",
+    "gauche3",
+    "gauche4"
+  );
+  p3.classList.remove(
+    "droite1",
+    "droite2",
+    "droite3",
+    "droite4",
+    "centre",
+    "gauche1",
+    "gauche2",
+    "gauche3",
+    "gauche4"
+  );
+  p4.classList.remove(
+    "droite1",
+    "droite2",
+    "droite3",
+    "droite4",
+    "centre",
+    "gauche1",
+    "gauche2",
+    "gauche3",
+    "gauche4"
+  );
+  p5.classList.remove(
+    "droite1",
+    "droite2",
+    "droite3",
+    "droite4",
+    "centre",
+    "gauche1",
+    "gauche2",
+    "gauche3",
+    "gauche4"
+  );
+  po1.classList.remove(grand);
+  po2.classList.remove(grand);
+  po3.classList.remove(grand);
+  po4.classList.remove(grand);
+  po5.classList.remove(grand);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  reset();
-  p1.classList.add("droite2");
-  p2.classList.add("droite1");
-  p3.classList.add("centre");
-  p4.classList.add("gauche1");
-  p5.classList.add("gauche2");
-
+  pointCount();
 });
 
-document.addEventListener("click")
-
 // valeur du content qui log le placement
-function pointCount () {
+function pointCount() {
   reset();
 
   if (y === 1) {
@@ -50,19 +96,19 @@ function pointCount () {
     p3.classList.add("gauche1");
     p4.classList.add("gauche2");
     p5.classList.add("gauche3");
-  } else if ( y === 3 ) {
+  } else if (y === 3) {
     p1.classList.add("droite2");
     p2.classList.add("droite1");
     p3.classList.add("centre");
     p4.classList.add("gauche1");
     p5.classList.add("gauche2");
-  } else if ( y === 4 ) {
+  } else if (y === 4) {
     p1.classList.add("droite3");
     p2.classList.add("droite2");
     p3.classList.add("droite1");
     p4.classList.add("centre");
     p5.classList.add("gauche1");
-  } else if ( y === 5 ) {
+  } else if (y === 5) {
     p1.classList.add("droite4");
     p2.classList.add("droite3");
     p3.classList.add("droite2");
